2d660d5c6f4557d08a9ecc647cee52c0d86be481
Chris Dock <cbartondock@gmail.com>
2015-05-08 15:58:30 -0700

    final version

--- control.py
+++ control.py
@@ -1,13 +1,14 @@
 import wiringpi2 as wp
+import MySQLdb
+import time
+from getch import getch
 wp.wiringPiSetupPhys()
-
-
-rpins =[29,31,33,35,37]
-tpins=[12,11,13,15,16]
-
+readlatest = """SELECT t1.* FROM SENSORS t1
+WHERE t1.TIME = (SELECT MAX(t2.TIME) FROM SENSORS t2)"""
+rpins = [29,31,33,35,37]
+tpins = [12,11,13,15,16]
 ron =[0,0,0,0,0]
 ton =[0,0,0,0,0]
-
 for rpin in rpins:
     print(rpin)
     wp.pinMode(rpin,1)
@@ -18,29 +19,26 @@ for tpin in tpins:
     wp.pinMode(tpin,1)
     wp.digitalWrite(tpin,0)
 print("\n")
-thrust=0
-thrustmax=31
-rot=0
-rotmax=31
-
-
+thrust, thrustmax = 0, 31
+rot, rotmax = 0, 31
 bitarray32 = lambda n: map(int,list("0"*(5-len(bin(n)[2:]))+bin(n)[2:]))
-
 while True:
-    key = raw_input("-->")
-    print("keypress")
-    if key =="w":
-        if thrust <thrustmax:
-            thrust+=1
-    if key== "s":
-        if thrust>0:
-            thrust-=1
-    if key =="d":
-        if rot < rotmax:
-            rot+=1
-    if key=="a":
-        if rot>0:
-            rot-=1
+    key = getch()
+    if key =="e":
+        db=MySQLdb.connect("sql4.freesqldatabase.com","sql476380","eW4%sG5%","sql476380")
+	    time.sleep(.00009)
+	    cursor=db.cursor()
+	    cursor.execute(readlatest)
+	    print("SENSORS: "+str(cursor.fetchone()))
+	    db.close()
+    if key =="w" and thrust<thrustmax:
+        thrust+=1
+    if key== "s" and thrust>0:
+        thrust-=1
+    if key =="d" and rot<rotmax:
+        rot+=1
+    if key=="a" and rot>0:
+        rot-=1
     ton = bitarray32(thrust)
     ron = bitarray32(rot)
     for i in range(0,5):
@@ -48,15 +46,3 @@ while True:
         wp.digitalWrite(rpins[i],ron[i])
     print("thrust is: " + str(thrust))
     print("rot is: " + str(rot))
-"""
-check=0
-wp.pinMode(7,1)
-while True:
-    key = raw_input("--<")
-    if key =="e":
-        check = 1- check
-        wp.digitalWrite(7,check)
-    wp.pinMode(7,0)
-    print("read: ",str(wp.digitalRead(7)))
-    wp.pinMode(7,1)
-"""
--- pintest.py
+++ pintest.py
@@ -4,15 +4,13 @@ wp.wiringPiSetupPhys()
 tpin=7
 wp.pinMode(tpin,1)
 while True:
-    print("hi")
-    
     time.sleep(1)
     wp.digitalWrite(tpin,0)
     wp.pinMode(tpin,0)
     print("read off: ",wp.digitalRead(tpin))
-    wp.pinMode(tpin,1)    
+    wp.pinMode(tpin,1)
     time.sleep(1)
-    wp.digitalWrite(tpin,1) 
+    wp.digitalWrite(tpin,1)
     wp.pinMode(tpin,0)
     print("read on: ",wp.digitalRead(tpin))
     wp.pinMode(tpin,1)
